c7db33125e11ede66032ae3f2b967e1f0f8a9067
Mateusz Kowalkowski <mateusz.kowalkowski@gmail.com>
2023-10-12 15:43:07 +0200

    I feel like a child discovering ice cream for the first time

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,12 @@ struct Color {
     a: f64,
 }
 
+#[derive(PartialEq, Eq)]
+enum PipelineAlternatives {
+    Default,
+    Alternative1,
+}
+
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
@@ -22,6 +28,8 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     bg_color: Color,
+    pipeline_alternative: PipelineAlternatives,
+    render_pipeline: wgpu::RenderPipeline,
     // The window must be declared after the surface so
     // it gets dropped after it as the surface contains
     // unsafe references to the window's resources.
@@ -95,6 +103,58 @@ impl State {
                             a: 1.0,
                         };
 
+        let pipeline_alternative = PipelineAlternatives::Default;
+
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: "vs_main", // 1.
+                buffers: &[], // 2.
+            },
+            fragment: Some(wgpu::FragmentState { // 3.
+                module: &shader,
+                entry_point: "fs_main",
+                targets: &[Some(wgpu::ColorTargetState { // 4.
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None, // 1.
+                multisample: wgpu::MultisampleState {
+                    count: 1, // 2.
+                    mask: !0, // 3.
+                    alpha_to_coverage_enabled: false, // 4.
+                },
+                multiview: None, // 5.
+            });
+
+
+
+
         Self {
             window,
             surface,
@@ -103,6 +163,8 @@ impl State {
             config,
             size,
             bg_color,
+            pipeline_alternative,
+            render_pipeline,
         }
 
     }
@@ -132,6 +194,107 @@ impl State {
                 self.bg_color.r = normalized_x;
                 self.bg_color.g = normalized_y;
             },
+            WindowEvent::KeyboardInput { 
+                input:
+                    KeyboardInput { 
+                        state: ElementState::Pressed, 
+                        virtual_keycode: Some(VirtualKeyCode::Space), 
+                        ..
+                    },
+                ..
+            } => {
+
+                let device = &self.device;
+                let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+                let render_pipeline_layout =
+                    device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                        label: Some("Render Pipeline Layout"),
+                        bind_group_layouts: &[],
+                        push_constant_ranges: &[],
+                    });
+                let render_pipeline: wgpu::RenderPipeline;
+
+                if self.pipeline_alternative == PipelineAlternatives::Default {
+                    self.pipeline_alternative = PipelineAlternatives::Alternative1;
+                    render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                        label: Some("Render Pipeline"),
+                        layout: Some(&render_pipeline_layout),
+                        vertex: wgpu::VertexState {
+                            module: &shader,
+                            entry_point: "vs_main", // 1.
+                            buffers: &[], // 2.
+                        },
+                        fragment: Some(wgpu::FragmentState { // 3.
+                            module: &shader,
+                            entry_point: "fs_main",
+                            targets: &[Some(wgpu::ColorTargetState { // 4.
+                                format: self.config.format,
+                                blend: Some(wgpu::BlendState::REPLACE),
+                                write_mask: wgpu::ColorWrites::ALL,
+                            })],
+                        }),
+                        primitive: wgpu::PrimitiveState {
+                            topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                            strip_index_format: None,
+                            front_face: wgpu::FrontFace::Ccw, // 2.
+                            cull_mode: Some(wgpu::Face::Back),
+                            // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                            polygon_mode: wgpu::PolygonMode::Fill,
+                            // Requires Features::DEPTH_CLIP_CONTROL
+                            unclipped_depth: false,
+                            // Requires Features::CONSERVATIVE_RASTERIZATION
+                            conservative: false,
+                        },
+                        depth_stencil: None, // 1.
+                            multisample: wgpu::MultisampleState {
+                                count: 1, // 2.
+                                mask: !0, // 3.
+                                alpha_to_coverage_enabled: false, // 4.
+                            },
+                            multiview: None, // 5.
+                        });
+                } else {
+                    self.pipeline_alternative = PipelineAlternatives::Default;
+                    render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                        label: Some("Render Pipeline"),
+                        layout: Some(&render_pipeline_layout),
+                        vertex: wgpu::VertexState {
+                            module: &shader,
+                            entry_point: "vs_main", // 1.
+                            buffers: &[], // 2.
+                        },
+                        fragment: Some(wgpu::FragmentState { // 3.
+                            module: &shader,
+                            entry_point: "fs2_main",
+                            targets: &[Some(wgpu::ColorTargetState { // 4.
+                                format: self.config.format,
+                                blend: Some(wgpu::BlendState::REPLACE),
+                                write_mask: wgpu::ColorWrites::ALL,
+                            })],
+                        }),
+                        primitive: wgpu::PrimitiveState {
+                            topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                            strip_index_format: None,
+                            front_face: wgpu::FrontFace::Ccw, // 2.
+                            cull_mode: Some(wgpu::Face::Back),
+                            // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                            polygon_mode: wgpu::PolygonMode::Fill,
+                            // Requires Features::DEPTH_CLIP_CONTROL
+                            unclipped_depth: false,
+                            // Requires Features::CONSERVATIVE_RASTERIZATION
+                            conservative: false,
+                        },
+                        depth_stencil: None, // 1.
+                            multisample: wgpu::MultisampleState {
+                                count: 1, // 2.
+                                mask: !0, // 3.
+                                alpha_to_coverage_enabled: false, // 4.
+                            },
+                            multiview: None, // 5.
+                        });
+                }
+                self.render_pipeline = render_pipeline;
+            },
             _ => ()
         }
         false
@@ -148,7 +311,7 @@ impl State {
         });
 
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
@@ -165,6 +328,8 @@ impl State {
                 })],
                 depth_stencil_attachment: None,
             });
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
 
         // submit will accept anything that implements IntoIter
@@ -202,6 +367,17 @@ pub async fn run() {
                         ..
                     } => *control_flow = ControlFlow::Exit,
                     WindowEvent::CursorMoved { .. } => {
+                        state.input(event);
+                    },
+                    WindowEvent::KeyboardInput { 
+                        input:
+                            KeyboardInput { 
+                                state: ElementState::Pressed, 
+                                virtual_keycode: Some(VirtualKeyCode::Space), 
+                                ..
+                            },
+                        ..
+                    } => {
                         state.input(event);
                     },
                     WindowEvent::Resized(physical_size) => {
